4922b17a5b38114cf1957408f76e88cde2a12153
longxingyu <longxingyu@halolongs-MacBook-Air.local>
2018-03-11 21:19:41 +0800

    Updating the first week course and codes showing in the class.

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by longxingyu on 11/03/2018.
+//  Copyright © 2018 longxingyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips:\(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiChoices = ["💩","👽","👽","💩"]
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardNumber=\(cardNumber)")
+        } else {
+            print("choose card was not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Developing-iOS-11-Apps-with-Swift-
+# Developing-iOS-11-Apps-with-Swift
 Implement project code in this course
+
+The following would show what I have already done, you could check! (I will be update in every Sunday)
+
+* week-1( 11th Mar)
+  * video: [Introduction to iOS 11, Xcode 9 and Swift 4](https://www.bilibili.com/video/av16339375/index_2.html#page=1) (in bilibili)
+  * project1: Concentration
+
+
+
+
+
+ 
+
+
+
